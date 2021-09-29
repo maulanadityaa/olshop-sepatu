790a95c273ff5250518834f3e07959af1919f300
maulanadityaa <maulanadityaaa@gmail.com>
2021-09-30 00:54:01 +0700

    db migrate

--- database/migrations/2021_09_29_173339_create_belanjas_table.php
+++ database/migrations/2021_09_29_173339_create_belanjas_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateBelanjasTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,14 +13,12 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('belanjas', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->integer('level');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->integer('user_id');
+            $table->integer('produk_id');
+            $table->integer('total_harga');
+            $table->integer('status');
             $table->timestamps();
         });
     }
@@ -32,6 +30,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('belanjas');
     }
 }
