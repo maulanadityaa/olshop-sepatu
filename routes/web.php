821d1179a0378f5ad2c7f2d7b2056d7a235ae1c4
maulanadityaa <maulanadityaaa@gmail.com>
2021-10-29 12:07:29 +0700

    change config verify for heroku

--- routes/web.php
+++ routes/web.php
@@ -17,14 +17,14 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Auth::routes(['verify' => true]);
+Auth::routes(['verify' => false]);
 
 Route::get('/', \App\Http\Livewire\Home::class);
 Route::get('/tambah-produk', \App\Http\Livewire\TambahProduk::class);
 Route::get('/admin/dashboard', \App\Http\Livewire\DashboardAdmin::class)->name('dashboard');
 Route::get('/admin/pesanan/', \App\Http\Livewire\AdminPesanan::class)->name('admin-pesanan');
 Route::get('/edit-produk/{id}', \App\Http\Livewire\EditProduk::class)->name('edit-produk');
-Route::get('/keranjang', \App\Http\Livewire\BelanjaUser::class)->name('keranjang')->middleware(['verified']);
+Route::get('/keranjang', \App\Http\Livewire\BelanjaUser::class)->name('keranjang');
 Route::get('/tambah-ongkir/{id}', \App\Http\Livewire\TambahOngkir::class)->name('tambah-ongkir');
 Route::get('/keranjang/checkout/{id}', \App\Http\Livewire\Checkout::class)->name('checkout');
 Route::get('/keranjang/status/{id}', \App\Http\Livewire\StatusPesanan::class)->name('status');
